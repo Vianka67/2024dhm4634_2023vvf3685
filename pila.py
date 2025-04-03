9540e0c5a99cd82fb0b2b5e6e783c6525d362bf9
Vianka67 <viankavacaflores@gmail.com>
2025-04-02 21:39:58 -0400

    comi2

--- pila.py
+++ pila.py
@@ -6,3 +6,11 @@ class PilaEnlazada:
     def __init__(self):
         self.cima=None
         self.longitud=0
+    def esta_vacia(self):
+        return self.cima is None
+    
+    def apilar(self, valor):
+        nuevo_nodo= nodo(valor)
+        nuevo_nodo.siguiente = self.cima
+        self.cima = nuevo_nodo
+        self.longitud += 1
